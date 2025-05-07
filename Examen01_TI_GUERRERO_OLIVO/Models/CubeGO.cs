c54a27383e4609896365d3ee2ec4ea0990d35bd8
Salome001 <palomaolivo10@gmail.com>
2025-05-07 20:02:08 +0100

    Eliminado

---
+++ Examen01_TI_GUERRERO_OLIVO/Models/CubeGO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Examen01_TI_GUERRERO_OLIVO.Interfaces;
+
+namespace Examen01_TI_GUERRERO_OLIVO.Models
+{
+    public class CubeGO: ICalculateAreaGO, ICalculateVolumeGO
+    {
+        public double edge;
+        public CubeGO(double edge) => this.edge = edge; 
+      public double CalculateVolumeGO() => Math.Pow(edge,3);
+       
+        public double CalculateAreaGO() =>  6 * edge* edge;
+        
+        
+    }
+}
